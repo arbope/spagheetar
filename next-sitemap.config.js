72b4df4d4e93763e8a938da0210e91a7d14f826f
arbope <arbope3@gmail.com>
2025-05-28 21:42:08 +0200

    sitemap and robot.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://spagheetar.vercel.app', // Change this to your live URL
+    generateRobotsTxt: true, // Also creates robots.txt
+  }
+  
