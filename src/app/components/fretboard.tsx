5a83aed8e3307d6b2b55bf41b160accc03c5a957
arbope <arbope3@gmail.com>
2026-04-15 01:37:08 +0200

    better

--- src/app/components/fretboard.tsx
+++ src/app/components/fretboard.tsx
@@ -176,8 +176,8 @@ const Fretboard: React.FC<FretboardProps> = ({
 				<div
 					className="absolute px-2 py-1 rounded text-xl text-white bg-gray-800"
 					style={{
-						left: mousePos.x + 65,
-						top: mousePos.y + 15,
+						left: mousePos.x + 35,
+						top: mousePos.y + 30,
 						opacity: 0.6,
 						borderRadius: "8px",
 						pointerEvents: "none",
